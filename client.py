3bb7990f6ef00b0a358f9ae843bffc86cfcec13a
Laudkyle <kyleaby1@gmail.com>
2024-08-27 06:36:48 +0100

    few changes

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 import requests
 
-url = 'http://10.220.205.251:7000/verify'
-files = {'image': open('joe_test.jpg', 'rb')}
-response = requests.post(url, files=files)
-
-print(response.json())
+url = 'http://192.168.0.144:7000/verify'
+try:
+    with open('joe_test.jpg', 'rb') as img:
+        files = {'image': img}
+        response = requests.post(url, files=files, timeout=60)  
+    response.raise_for_status() 
+    print(response.json())
+except requests.exceptions.Timeout:
+    print("The request timed out")
+except requests.exceptions.ConnectionError:
+    print("Failed to connect to the server")
+except requests.exceptions.HTTPError as err:
+    print(f"HTTP error occurred: {err}")
+except Exception as err:
+    print(f"An error occurred: {err}")
